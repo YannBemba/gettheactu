95e9df6e4b5d6dd55ae35c56ca70b7d09167a710
Yann Bemba Longa <yannbembacontact@gmail.com>
2021-08-22 03:48:44 +0200

    Database and Room TypeConverter

--- app/src/main/java/com/example/gettheactu/database/ArticleDatabase.kt
+++ app/src/main/java/com/example/gettheactu/database/ArticleDatabase.kt
@@ -1,4 +1,35 @@
 package com.example.gettheactu.database
 
-class ArticleDatabase {
+import android.content.Context
+import androidx.room.*
+import com.example.gettheactu.models.Article
+
+@Database(
+    entities = [Article::class],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class ArticleDatabase: RoomDatabase() {
+
+    abstract fun getArticleDao(): ArticleDAO
+
+
+    companion object {
+        @Volatile
+        private var instance: ArticleDatabase? = null
+        private val LOCK = Any() // Syncronize single instance
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { it ->
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            ArticleDatabase::class.java,
+            "article_db.db"
+        ).build()
+
+    }
 }
