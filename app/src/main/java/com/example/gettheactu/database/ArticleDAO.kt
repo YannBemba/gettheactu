2e00a09e0184dabbd7d65b0bf2a45c5fbe5d4dfb
Yann Bemba Longa <yannbembacontact@gmail.com>
2021-08-22 03:34:19 +0200

    DAO + Article Setup w Room

--- app/src/main/java/com/example/gettheactu/database/ArticleDAO.kt
+++ app/src/main/java/com/example/gettheactu/database/ArticleDAO.kt
@@ -1,4 +1,19 @@
 package com.example.gettheactu.database
 
-class ArticleDAO {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.gettheactu.models.Article
+
+@Dao
+interface ArticleDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
 }
